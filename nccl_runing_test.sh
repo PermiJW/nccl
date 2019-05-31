b1039f47707a25e081e18dd8a1457adf98b28342
PermiJW <512183891@qq.com>
2019-05-31 12:12:07 -0700

    test

--- nccl_runing_test.sh
+++ nccl_runing_test.sh
@@ -5,5 +5,6 @@ cd nccl;\
 make -j src.build BUILDDIR=”/mount/jiawei-space/nccl_building/nccl_make/”;\
 cd ..;\
 cd nccl-tests;\
+rm -r build;\
 make NCCL_HOME=”/mount/jiawei-space/nccl_building/nccl_make/”;\
 ./build/all_reduce_perf -b 8 -e 128M -f 2 -g 4
